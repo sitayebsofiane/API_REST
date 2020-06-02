2c7c85bb54ca247346805a6ed21fed2323c2b904
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-06-02 16:43:30 +0200

    config bdd

--- config/Database.php
+++ config/Database.php
@@ -13,7 +13,7 @@ class Database{
         $this->connexion = null;
 
         try{
-            $this->connexion = new PDO("pgsql:host" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->connexion = new  PDO("pgsql:host=".$this->host.";dbname=".$this->db_name,$this->username,$this->password);;
             $this->connexion->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Erreur de connexion : " . $exception->getMessage();
